28d04546bd77c78b6aa29b35255c24a7492a478d
hideaki <>
2022-11-14 12:53:10 +0900

    add ex_real

--- HidKim_APP/parameter_initializer.py
+++ HidKim_APP/parameter_initializer.py
@@ -5,7 +5,7 @@ def parameter_initializer(datum,obs,cov_datum):
     var_rate = 1.
     for i in range(len(obs)):
         bins      = np.linspace(obs[i][0],obs[i][1],10)
-        rate, _   = np.histogram(datum[:,i],bins=bins)
+        rate, _   = np.histogram(np.array(datum)[:,i],bins=bins)
         rate      = rate / (bins[1]-bins[0])
         var_rate *= pow(np.var(np.sqrt(rate)),1./len(obs))
     set_par = [var_rate]+list(prec)
